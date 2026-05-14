43069016603a37a51e934cb63781e1838f51e3aa
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-05-14 15:04:18 +0700

    Menambahkan implementasi metode display pada kelas stack untuk menampilkan semua nilai dalam stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -55,3 +55,13 @@ public:
             cout << "List is empty." << endl;
         }
          else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
