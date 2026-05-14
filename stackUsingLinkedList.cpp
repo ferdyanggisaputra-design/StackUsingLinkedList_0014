0e0280a3dbccd656c299b424187b1e10a506552b
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-05-14 15:10:36 +0700

    Menambahkan opsi untuk memasukkan nilai yang akan dipush ke stack dalam fungsi main

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -90,3 +90,8 @@ int main()
 
         switch (choice)
         {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
